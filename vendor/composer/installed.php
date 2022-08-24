8e019dd1ebddc2b42363dae1b9568d0568679a3a
Tanner Moushey <tanner@missionlab.dev>
2022-08-23 23:27:12 -0500

    Add vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '51e143788aafac6ac6c8952f276c99081db08162',
+    'name' => 'churchplugins/cp-groups',
+  ),
+  'versions' => 
+  array (
+    'churchplugins/cp-groups' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '51e143788aafac6ac6c8952f276c99081db08162',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v2.1.1',
+      'version' => '2.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af93ba6e52236418f07a278033eba6959ee5b983',
+    ),
+    'wpackio/enqueue' => 
+    array (
+      'pretty_version' => '3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8a5604f5ab3bd5f000e503dda02603e15e534d47',
+    ),
+  ),
+);
