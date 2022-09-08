a82a33b18cbca64f5349bae459980e0799e9351c
Tanner Moushey <tanner@missionlab.dev>
2022-09-08 11:36:00 -0500

    Remove extra JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,14 @@ window.cpGroupsFilter = window.cpGroupsFilter || {};
 jQuery(function ($) {
 	
 	$(document).ready(function() {
-		$('.cp-group-item').on('click', '.cp-group-item--title a, .cp-group-item--thumb',function(e) {
+		
+		let $groupItem = $('.cp-group-item');
+		
+		if ( ! $groupItem.length ) {
+			return;
+		}
+		
+		$groupItem.on('click', '.cp-group-item--title a, .cp-group-item--thumb',function(e) {
 			e.preventDefault();
 			
 			let $this = $(this);
@@ -32,43 +39,11 @@ jQuery(function ($) {
 					});
 
 				},
-//				close        : function () {
-//					history.replaceState(null, null, '/integrations/');
-//					document.title = 'SmartVault Integration Center - Accounting, Tax, e-Signature, Fetching and More';
-//				}
 			});
 
 			$modalElem.dialog('open');
 			
 		});
 	} );
-	
-	var groupsFilter = function() {
-		var SELF = this;
-		
-		SELF.init = function() {
-			
-			SELF.$filterCont = $('.cp-groups-filter');
-			
-			if ( ! SELF.$filterCont.length ) {
-				return;
-			}
-
-			SELF.$archive = $('.cp-groups-archive');
-			SELF.$form = SELF.$filterCont.find('form');
-			
-			SELF.$form.on('submit', 'handleFilter');
-		};
-		
-		SELF.handleFilter = function(e) {
-			e.preventDefault();
-		}
-
-	}
-
-	window.cpGroupsFilter = new groupsFilter();
-	$(document).ready( function(){
-		window.cpGroupsFilter.init();
-	});
 	
 });
