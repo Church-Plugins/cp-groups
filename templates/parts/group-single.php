0dd74dd42e72549e9cd9b9fa0723a9570a0413da
Jonathan Roley <jonathan.roley@gmail.com>
2023-08-23 09:11:26 -0400

    remove class from tags

--- templates/parts/group-single.php
+++ templates/parts/group-single.php
@@ -58,7 +58,7 @@ try {
 				<?php endif; ?>
 
 				<?php if( $cp_connect_tags = get_option( 'cp_group_custom_meta_mapping', false ) ) : ?>
-					<div class="cp-group-single--type">
+					<div>
 						<?php foreach( $cp_connect_tags as $tag ) : ?>
 							<?php $item_tag = get_post_meta( $item['originID'], $tag['slug'], true ) ?>
 							<?php if( empty( $item_tag ) ) continue; ?>
