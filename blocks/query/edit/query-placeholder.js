74b4d6b277f5ef82ad41c09e15e0a25aa37f74f4
Jonathan Roley <jonathan.roley@gmail.com>
2023-10-12 13:34:37 -0400

    set up prebuilt block patterns

--- blocks/query/edit/query-placeholder.js
+++ blocks/query/edit/query-placeholder.js
@@ -1,17 +1,14 @@
 /**
  * WordPress dependencies
  */
-import { useSelect, useDispatch } from '@wordpress/data';
+import { useSelect, withDispatch } from '@wordpress/data';
 import {
 	createBlocksFromInnerBlocksTemplate,
 	store as blocksStore,
 } from '@wordpress/blocks';
-import { useState } from '@wordpress/element';
 import {
 	useBlockProps,
-	store as blockEditorStore,
-	__experimentalBlockVariationPicker,
-	__experimentalGetMatchingVariation as getMatchingVariation,
+	store as blockEditorStore
 } from '@wordpress/block-editor';
 import { Button, Placeholder } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
@@ -19,31 +16,34 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { useScopedBlockVariations, useBlockNameForPatterns } from '../utils';
+import { useBlockNameForPatterns } from '../utils';
 
-export default function QueryPlaceholder( {
+const DEFAULT_TEMPLATE = [
+	[ 'cp-groups/group-template' ]
+];
+
+function QueryPlaceholder( {
 	attributes,
 	clientId,
 	name,
 	openPatternSelectionModal,
-	setAttributes,
+	startBlank
 } ) {
-	const [ isStartingBlank, setIsStartingBlank ] = useState( false );
+
 	const blockProps = useBlockProps();
 	const blockNameForPatterns = useBlockNameForPatterns(
 		clientId,
 		attributes
 	);
 
-	const { blockType, allVariations, hasPatterns } = useSelect(
+	const { blockType, hasPatterns } = useSelect(
 		( select ) => {
-			const { getBlockVariations, getBlockType } = select( blocksStore );
-			const { getBlockRootClientId, getPatternsByBlockTypes } =
-				select( blockEditorStore );
+			const { getBlockType } = select( blocksStore );
+			const { getBlockRootClientId, getPatternsByBlockTypes } = select( blockEditorStore );
 			const rootClientId = getBlockRootClientId( clientId );
+
 			return {
 				blockType: getBlockType( name ),
-				allVariations: getBlockVariations( name ),
 				hasPatterns: !! getPatternsByBlockTypes(
 					blockNameForPatterns,
 					rootClientId
@@ -53,23 +53,9 @@ export default function QueryPlaceholder( {
 		[ name, blockNameForPatterns, clientId ]
 	);
 
-	const matchingVariation = getMatchingVariation( attributes, allVariations );
-	const icon =
-		matchingVariation?.icon?.src ||
-		matchingVariation?.icon ||
-		blockType?.icon?.src;
-	const label = matchingVariation?.title || blockType?.title;
-	if ( isStartingBlank ) {
-		return (
-			<QueryVariationPicker
-				clientId={ clientId }
-				attributes={ attributes }
-				setAttributes={ setAttributes }
-				icon={ icon }
-				label={ label }
-			/>
-		);
-	}
+	const icon  = blockType?.icon?.src;
+	const label = blockType?.title;
+
 	return (
 		<div { ...blockProps }>
 			<Placeholder
@@ -90,9 +76,7 @@ export default function QueryPlaceholder( {
 
 				<Button
 					variant="secondary"
-					onClick={ () => {
-						setIsStartingBlank( true );
-					} }
+					onClick={ startBlank }
 				>
 					{ __( 'Start blank' ) }
 				</Button>
@@ -101,46 +85,16 @@ export default function QueryPlaceholder( {
 	);
 }
 
-function QueryVariationPicker( {
-	clientId,
-	attributes,
-	setAttributes,
-	icon,
-	label,
-} ) {
-	const scopeVariations = useScopedBlockVariations( attributes );
-	const { replaceInnerBlocks } = useDispatch( blockEditorStore );
-	const blockProps = useBlockProps();
-	return (
-		<div { ...blockProps }>
-			<__experimentalBlockVariationPicker
-				icon={ icon }
-				label={ label }
-				variations={ scopeVariations }
-				onSelect={ ( variation ) => {
-					if ( variation.attributes ) {
-						setAttributes( {
-							...variation.attributes,
-							query: {
-								...variation.attributes.query,
-								postType:
-									attributes.query.postType ||
-									variation.attributes.query.postType,
-							},
-							namespace: attributes.namespace,
-						} );
-					}
-					if ( variation.innerBlocks ) {
-						replaceInnerBlocks(
-							clientId,
-							createBlocksFromInnerBlocksTemplate(
-								variation.innerBlocks
-							),
-							false
-						);
-					}
-				} }
-			/>
-		</div>
-	);
-}
+export default withDispatch(( dispatch, { clientId } ) => {
+	const { replaceInnerBlocks } = dispatch( blockEditorStore );
+
+	return {
+		startBlank: () => {
+			replaceInnerBlocks(
+				clientId,
+				createBlocksFromInnerBlocksTemplate( DEFAULT_TEMPLATE ),
+				false
+			)
+		}
+	}
+})(QueryPlaceholder)
--- includes/Setup/Blocks/Init.php
+++ includes/Setup/Blocks/Init.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Initializes Gutenberg blocks
+ *
+ * @package CP_Groups
+ */
 
 namespace CP_Groups\Setup\Blocks;
 
@@ -10,6 +15,8 @@ use CP_Groups\Admin\Settings;
 class Init {
 
 	/**
+	 * Class instance
+	 *
 	 * @var Init
 	 */
 	protected static $_instance;
@@ -43,8 +50,8 @@ class Init {
 	 * @return void
 	 */
 	protected function includes() {
-    new Query();
-	  new GroupTitle();
+		new Query();
+		new GroupTitle();
 		new GroupTemplate();
 		new GroupFeaturedImage();
 		new GroupExcerpt();
@@ -53,7 +60,7 @@ class Init {
 		new GroupTimeDesc();
 		new GroupContent();
 		new GroupBadge();
-  }
+	}
 
 	/**
 	 * Plugin init actions
@@ -61,20 +68,46 @@ class Init {
 	 * @return void
 	 */
 	protected function actions() {
-		add_filter( 'default_post_metadata', [ $this, 'default_thumbnail' ], 10, 5 );
+		add_action( 'init', array( $this, 'register_block_patterns' ) );
+		add_filter( 'default_post_metadata', array( $this, 'default_thumbnail' ), 10, 5 );
 	}
 
+	/**
+	 * Set default thumbnail for groups
+	 *
+	 * @param mixed  $value     The value get_metadata() should return - a single metadata value.
+	 * @param int    $object_id Object ID.
+	 * @param string $meta_key  Meta key.
+	 * @param bool   $single    Whether to return only the first value of the specified $meta_key.
+	 * @param string $meta_type Type of object metadata is for (e.g., comment, post, or user).
+	 */
 	public function default_thumbnail( $value, $object_id, $meta_key, $single, $meta_type ) {
-		if( $meta_key === '_thumbnail_id' && $meta_type === 'post' ) {
+		if ( 'thumbnail_id' === $meta_key && 'post' === $meta_type ) {
 			$post_type = get_post_type( $object_id );
-			if( $post_type === cp_groups()->setup->post_types->groups->post_type ) {
+			if ( cp_groups()->setup->post_types->groups->post_type === $post_type ) {
 				$image = Settings::get( 'default_thumbnail' );
-				if( $image ) {
+				if ( $image ) {
 					$image = attachment_url_to_postid( $image );
-				} 
+				}
 				return $image ? $image : $value;
 			}
 		}
 		return $value;
+	}
+
+	/**
+	 * Register block patterns
+	 */
+	public function register_block_patterns() {
+		$patterns_dir = CP_GROUPS_PLUGIN_DIR . 'block-patterns/';
+
+		$files = glob( $patterns_dir . '*.php' );
+
+		foreach ( $files as $file ) {
+			register_block_pattern(
+				'cp-groups/' . basename( $file, '.php' ),
+				require $file
+			);
+		}
 	}
 }
